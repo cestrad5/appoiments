78f7515692cb70230e099c010b51b6351eb435ef
CAMILO <cestrad5@gmail.com>
2023-07-09 11:11:35 -0500

    Setting the project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+
+function App() {
+
+    const ege = 15
+
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+
+    return (
+
+        <h1>Desde el Header</h1>
+    )
+}
+
+export default Header
